b562c416edfcc631ffddd9d670ff49e63ab29496
impact-mjodgn <your-email@example.com>
2023-10-16 19:30:20 -0500

    updated comments; small readibility improvements

--- grep.py
+++ grep.py
@@ -10,42 +10,42 @@ def grep(pattern, flags, files):
 
     for file in files: #iterate through files
 
-        f = open(file, 'r') #open and read in file
+        f = open(file, 'r') #open and read file
         lines = f.readlines() 
 
-        for lineNum, line in enumerate(lines): #iterate through file lines
+        for lineNum, line in enumerate(lines): #iterate through file lines and retain line number
 
 
             if '-i' in flaglist: #flag for case-insensitive comparison
                 newPat = pattern.lower()
                 newLine = line.lower()
-            else: #newPat and newLine needed to retain original line case-sensitivity for output
+            else: #newPat and newLine added to retain original line case-sensitivity for output
                 newPat = pattern
                 newLine = line
 
 
-            flag = 0 #default flag set to 0; when -x active and pattern doesn't match full line, flag is set to 1 and line is not added to results
+            matchLine = 0 #default flag set to 0; when -x active and pattern doesn't match full line, flag is set to 1 and line is not added to results
             if '-x' in flaglist:
                 if not newPat == newLine.strip():
-                    flag = 1
+                    matchLine = 1
 
-            if (newPat in newLine and flag == 0) ^ ('-v' in flaglist): #checks for invert flag with XOR
+            if (newPat in newLine and matchLine == 0) ^ ('-v' in flaglist): #checks for invert flag with XOR
 
-                if '-l' in flaglist: #flag for only printing names of files
+                if '-l' in flaglist: #flag for only printing names of files; if a match is found, we go on to next file
                     results.append(f"{file}\n")
-                    break #if a match is found, we go on to next file
+                    break 
 
                 elif '-n' in flaglist: #flag for including line number
                     results.append(f"{file}:{lineNum+1}:{line}")
                 
                 else: #default 
                     results.append(f"{file}:{line}")
-        f.close()
+        f.close() #close file
 
     if len(files) < 2 and '-l' not in flaglist: #when there is only a single file and we aren't just printing file names ('-l' flag) we remove the file name prefix 
         for i in results:
-            tempRes.append(i.split(':', maxsplit=1)[1])
+            tempRes.append(i.split(':', 1)[1])
         results = tempRes
 
 
-    return ''.join(results)
+    return ''.join(results) 
